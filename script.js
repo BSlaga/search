9e15bd8bed48846a978b3521fd5519fdf5ee14da
BSlaga <148803209+BSlaga@users.noreply.github.com>
2024-01-17 16:08:32 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const suggestions = document.querySelector('.suggestions ul');
 const fruits = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
 
 function search(str) {
+	//filter the suggestions 
 	let results = [];
 	let inputLC = input.value.toLowerCase();
 	fruits.forEach(fruit => {
@@ -23,31 +24,36 @@ function searchHandler(e) {
 }
 
 function showSuggestions(results, inputVal) {
+	//remove suggestions everytime a key is pressed
 	suggestions.replaceChildren()
 
+	//create suggestion list
 	results.forEach(fruit => {
 		const newLi = document.createElement("li");
 		newLi.setAttribute('id',fruit);
 		newLi.innerText = fruit;
+		newLi.addEventListener('mouseover', (e)=> {});
 		suggestions.append(newLi);
 			
 	});
-
-	suggestions.setAttribute('class', 'has-suggestions')
-	//Make it so if empty or input.value isnt in results
-	if(input.value == ''){
-		suggestions.replaceChildren()
-		suggestions.removeAttribute('class','has-suggestions')
+	//removes dropdown when input is empty or has no suggestions
+	if(results.length > 0){
+		suggestions.setAttribute('class', 'has-suggestions');
+	}else{
+		suggestions.removeAttribute('class','has-suggestions');
 	}
-	//if(input.value == '' || !results.includes(input.value)){
-	//	suggestions.replaceChildren()
-	//	suggestions.removeAttribute('class','has-suggestions')
-	//}
-
+	
+	if(input.value == ''){
+		suggestions.replaceChildren();
+		suggestions.removeAttribute('class','has-suggestions');
+	}
+	
 }
 
 function useSuggestion(e) {
-	// TODO
+	input.value = e.target.innerText;
+	suggestions.replaceChildren();
+	suggestions.removeAttribute('class','has-suggestions');
 }
 
 input.addEventListener('keyup', searchHandler);
